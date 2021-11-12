f4f4a984f324c702bf3f8ed9d86e606df5de52e0
Sital Sitoula <heavywater221@gmail.com>
2021-11-12 01:16:19 -0600

    Added starter  security configs

--- reddit-clone-backend/src/main/java/com/example/redditclonebackend/config/SecurityConfig.java
+++ reddit-clone-backend/src/main/java/com/example/redditclonebackend/config/SecurityConfig.java
@@ -1,2 +1,29 @@
-package com.example.redditclonebackend.config;public class SecurityConfig {
+package com.example.redditclonebackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // csrf attacks occur in session. We are using JWT
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
