2562e48ad2a06adf392f229cf4ffeebbb43bae2d
Sital Sitoula <heavywater221@gmail.com>
2021-11-13 11:45:10 -0600

    Configured Spring Securiy use custom userDetailsService

--- reddit-clone-backend/src/main/java/com/example/redditclonebackend/config/SecurityConfig.java
+++ reddit-clone-backend/src/main/java/com/example/redditclonebackend/config/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.example.redditclonebackend.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // csrf attacks occur in session. We are using JWT
@@ -20,6 +29,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+
     }
 
     @Bean
--- reddit-clone-backend/src/main/java/com/example/redditclonebackend/config/security/SecurityUser.java
+++ reddit-clone-backend/src/main/java/com/example/redditclonebackend/config/security/SecurityUser.java
@@ -1,2 +1,57 @@
-package com.example.redditclonebackend.config.security;public class SecurityUser {
+package com.example.redditclonebackend.config.security;
+
+import com.example.redditclonebackend.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+import static java.util.Collections.singletonList;
+
+/**
+ * Wrapper class for User which implements UserDetails
+ */
+
+@AllArgsConstructor
+public class SecurityUser implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return singletonList(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
 }
