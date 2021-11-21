672665e5da30135c100becd5b06ad86c327ad054
Sital Sitoula <heavywater221@gmail.com>
2021-11-21 00:35:47 -0600

    Configured Swagger to view API docs

--- reddit-clone-backend/src/main/java/com/example/redditclonebackend/RedditCloneBackendApplication.java
+++ reddit-clone-backend/src/main/java/com/example/redditclonebackend/RedditCloneBackendApplication.java
@@ -1,12 +1,14 @@
 package com.example.redditclonebackend;
 
+import com.example.redditclonebackend.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync(proxyTargetClass = true)
-
+@Import(SwaggerConfiguration.class)
 public class RedditCloneBackendApplication {
 
     public static void main(String[] args) {
--- reddit-clone-backend/src/main/java/com/example/redditclonebackend/config/SecurityConfig.java
+++ reddit-clone-backend/src/main/java/com/example/redditclonebackend/config/SecurityConfig.java
@@ -41,6 +41,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers(HttpMethod.GET, "/api/subreddit")
                 .permitAll()
+                .antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated();
 
